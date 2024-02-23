796ab685017123648e6e80c5d7a529df86912709
JonibekCoder <jonibekhamroqulov2004@gmail.com>
2024-02-23 14:38:32 +0500

    Initial commit

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='list'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('create/', views.PostCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.PostUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='delete'),
+]
---
+++ posts/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django import forms
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'list.html'
+    context_object_name = 'posts'
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    template_name = 'create.html'
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("posts:detail", kwargs={'pk': self.object.pk})
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        form.fields['title'].widget.attrs['class'] = 'form-control'
+        form.fields['title'].widget.attrs['placeholder'] = 'Title'
+        form.fields['title'].label = ''
+
+        form.fields['body'].widget.attrs['class'] = 'form-control'
+        form.fields['body'].widget.attrs['placeholder'] = 'Body'
+        form.fields['body'].label = ''
+
+        return form
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Post
+    template_name = 'update.html'
+    context_object_name = 'post'
+    fields = ['title', 'body']
+
+    def test_func(self):
+        post = self.get_object()
+        user = self.request.user
+        if user == post.author:
+            return True
+        else:
+            return False
+
+    def get_success_url(self):
+        return reverse_lazy("posts:detail", kwargs={'pk': self.object.pk})
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Post
+    template_name = 'delete.html'
+    context_object_name = 'post'
+
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        user = self.request.user
+        if user == post.author:
+            return True
+        else:
+            return False
